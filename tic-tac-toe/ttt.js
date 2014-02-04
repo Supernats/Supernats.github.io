dac5db0d3df5ed38fd3a61efabfaa14da0662c42
Supernats <nathanseither@gmail.com>
2014-02-03 17:11:36 -0800

    Add towers and ttt

---
+++ tic-tac-toe/ttt.js
@@ -0,0 +1,161 @@
+// NB: This doesn't include any AI.
+
+(function (root) {
+
+  var TTT = root.TTT = (root.TTT || {});
+
+  var Game = TTT.Game = function TT() {
+    this.player = Game.marks[0];
+    this.board = this.makeBoard();
+    this.jQuerify($('#wrapper'));
+  }
+
+  Game.marks = ["X", "O"];
+
+  Game.prototype.diagonalWinner = function () {
+    var game = this;
+
+    var diagonalPositions1 = [[0, 0], [1, 1], [2, 2]];
+    var diagonalPositions2 = [[2, 0], [1, 1], [0, 2]];
+
+    var winner = null;
+    _(Game.marks).each(function (mark) {
+      function didWinDiagonal (diagonalPositions) {
+        return _.every(diagonalPositions, function (pos) {
+          return game.board[pos[0]][pos[1]] === mark;
+        });
+      }
+
+      var won = _.any(
+        [diagonalPositions1, diagonalPositions2],
+        didWinDiagonal
+      );
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+
+  Game.prototype.isEmptyPos = function (pos) {
+    return (this.board[pos[0]][pos[1]] === null);
+  };
+
+  Game.prototype.horizontalWinner = function () {
+    var game = this;
+
+    var winner = null;
+    _(Game.marks).each(function (mark) {
+      var indices = _.range(0, 3);
+
+      var won = _(indices).any(function (i) {
+        return _(indices).every(function (j) {
+          return game.board[i][j] === mark;
+        });
+      });
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+
+  Game.prototype.jQuerify = function (board) {
+    var game = this;
+    board.find('.row').each(function(rowIndex, rowEl) {
+      $(rowEl).find('.tile').each(function(colIndex, colEl) {
+        console.log(colEl);
+        $(colEl).data('coord', [rowIndex,colIndex]);
+        $(colEl).html(" ");
+      });
+    });
+    $('.tile').on('click', function() {
+      game.move($(this));
+    });
+  };
+
+  Game.prototype.makeBoard = function () {
+    return _.times(3, function (i) {
+      return _.times(3, function (j) {
+        return null;
+      });
+    });
+  };
+
+  Game.prototype.move = function (tile) {
+    if (!this.isEmptyPos(tile.data('coord'))) {
+      return false;
+    }
+
+    this.placeMark(tile);
+    this.switchPlayer();
+    return true;
+  };
+
+  Game.prototype.placeMark = function (tile) {
+    var game = this;
+    game.board[tile.data('coord')[0]][tile.data('coord')[1]] = game.player;
+    tile.text(game.player);
+    game.player == 'X' ? tile.addClass('red') : tile.addClass('green');
+
+    if (game.winner()) {
+      window.alert(game.player + ' wins! Get down with your bad self!');
+      $('.tile').off('click');
+    }
+  };
+
+  Game.prototype.switchPlayer = function () {
+    if (this.player === Game.marks[0]) {
+      this.player = Game.marks[1];
+    } else {
+      this.player = Game.marks[0];
+    }
+  };
+
+  Game.prototype.valid = function (pos) {
+    // Check to see if the co-ords are on the board and the spot is
+    // empty.
+
+    function isInRange (pos) {
+      return (0 <= pos) && (pos < 3);
+    }
+
+    return _(pos).all(isInRange) && _.isNull(this.board[pos[0]][pos[1]]);
+  };
+
+  Game.prototype.verticalWinner = function () {
+    var game = this;
+
+    var winner = null;
+    _(Game.marks).each(function (mark) {
+      var indices = _.range(0, 3);
+
+      var won = _(indices).any(function (j) {
+        return _(indices).every(function (i) {
+          return game.board[i][j] === mark;
+        });
+      });
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+
+  Game.prototype.winner = function () {
+    return (
+      this.diagonalWinner() || this.horizontalWinner() || this.verticalWinner()
+    );
+  };
+
+})(this);
+
+$(document).ready(function(){
+  var game = new TTT.Game();
+});
